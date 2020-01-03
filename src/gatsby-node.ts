0adc1f1065b32e32b1058b1273f4cdabf8bc2cc9
chris-bingham <chris.bingham@aerian.com>
2020-01-03 14:13:53 +0000

    Add plugins folder to includes config array

--- src/gatsby-node.ts
+++ src/gatsby-node.ts
@@ -45,6 +45,7 @@ const defaultOptions = {
   includes: [
     "./src/**/*.tsx",
     "./src/**/*.ts",
+    "./plugins/**/*.js",
     "./node_modules/gatsby-source-contentful/src/*.js",
     "./node_modules/gatsby-transformer-sharp/src/*.js",
     "./node_modules/gatsby-image/src/*.js"
