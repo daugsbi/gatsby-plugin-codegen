f89d7a54d6a6e6a857519baa1741ad809253c2b4
Denis Augsburger <denis.augsburger@twteam.ch>
2020-06-08 08:47:48 +0200

    Update package

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,23 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -56,14 +75,8 @@ var __spreadArrays = (this && this.__spreadArrays) || function () {
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.onPostBootstrap = void 0;
 var apollo_1 = require("apollo");
 var fs_1 = require("fs");
 var lodash_mergewith_1 = __importDefault(require("lodash.mergewith"));
@@ -89,7 +102,7 @@ var defaultOptions = {
         // "./node_modules/gatsby-*/**/*.js" Performance reasons
     ],
     // True can result in missed error messages through the console
-    // Set it the following way to catch the errors during build and still have watch mode:
+    // Set it the following way to catch the errors during the build and still have watch mode:
     // process.env.NODE_ENV === "development" ? true : false
     watch: false
 };
